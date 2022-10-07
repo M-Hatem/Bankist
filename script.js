3d68b401f5dd2afd754182b528694b75410be18c
MadoPlus <ma7modmado@gmail.com>
2022-10-07 15:39:08 +0200

    Added some logic and functions

--- script.js
+++ script.js
@@ -61,10 +61,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -163,6 +165,23 @@ btnLogin.addEventListener('click', e => {
     updateUI(currentAccount);
   });
 
+  btnLoan.addEventListener('click', e => {
+    e.preventDefault();
+
+    const amount = Number(inputLoanAmount.value);
+
+    if (
+      amount > 0 &&
+      currentAccount.movements.some(mov => mov >= 0.1 * amount)
+    ) {
+      currentAccount.movements.push(amount);
+
+      updateUI(currentAccount);
+    }
+
+    inputLoanAmount.value = '';
+  });
+
   btnClose.addEventListener('click', e => {
     e.preventDefault();
 
@@ -173,9 +192,10 @@ btnLogin.addEventListener('click', e => {
       const index = accounts.findIndex(
         acc => acc.username === currentAccount.username
       );
-      accounts.splice(index, 1);
 
+      accounts.splice(index, 1);
       containerApp.style.opacity = 0;
+
       window.scroll({
         top: 0,
         behavior: 'smooth',
@@ -184,6 +204,13 @@ btnLogin.addEventListener('click', e => {
 
     inputCloseUsername.value = inputClosePin.value = '';
   });
+});
+
+let sorted = false;
+btnSort.addEventListener('click', e => {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
 
 /////////////////////////////////////////////////
